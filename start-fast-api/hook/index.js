6994f78c89a097d1a057baa26de2f0feabb6cace
GuDongBin <ternpin@outlook.com>
2023-12-22 14:07:09 +0800

    first commit

---
+++ start-fast-api/hook/index.js
@@ -0,0 +1,25 @@
+async function preInit(inputObj) {
+    console.log(
+
+            `
+             .--.           .-.                     _ 
+: .-'          .' \`.                   :_;
+: \`;.--.   .--.\`. .'_____  .--.  .---. .-.
+: :' .; ; \`._-.': ::_____:' .; ; : .; \`: :
+:_;\`.__,_;\`.__.':_;       \`.__,_;: ._.':_;
+                                 : :      
+                                 :_;      
+
+
+            `
+    )
+}
+
+async function postInit(inputObj) {
+
+}
+
+module.exports = {
+    postInit,
+    preInit
+}
